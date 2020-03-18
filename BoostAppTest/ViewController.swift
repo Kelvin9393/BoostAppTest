097f97114c7a629805d242730b60f40c3dca5166
Kelvin <kelvin@fteg.com.my>
2020-03-18 20:19:07 +0800

    initial commit

---
+++ BoostAppTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BoostAppTest
+//
+//  Created by 2.9mac256 on 18/03/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .blue // TODO: Proof we have loaded
+    }
+
+
+}
+
